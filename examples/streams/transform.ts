ab691b6ea4429fe87d261828c103b7691e9b2dfc
slava.p <slava.p@techery.io>
2019-05-19 00:21:03 +0300

    Remove database connection and user tests

--- examples/streams/transform.ts
+++ examples/streams/transform.ts
@@ -20,27 +20,4 @@ class UpperCaseTransformer extends Transform {
 
 // fs.createReadStream(SOURCE_FILE).pipe(new UpperCaseTransformer()).pipe(process.stdout);
 
-const lastStream = fs.createReadStream(SOURCE_FILE).pipe(upperCaseTransformer).pipe(fs.createWriteStream(RESULT_FILE));
-
-/*
-
- // How to handle when pipeline finished
-const pipeline = new Promise((resolve, reject) => {
-    lastStream.on('finish',  resolve);
-    lastStream.on('error', reject);
-});
-
-pipeline.then(() => console.log('Completed'));
-
-
-// pipeline implementation for array of streams
-const pipeline = async streams => {
-    const streamChain = streams.reduce((chain, stream) => chain.pipe(stream));
-
-    return new Promise((resolve, reject) => {
-        streamChain.on('finish',  resolve);
-        streamChain.on('error', reject);
-    });
-};
-
-*/
+fs.createReadStream(SOURCE_FILE).pipe(upperCaseTransformer).pipe(fs.createWriteStream(RESULT_FILE));
--- features/users.feature
+++
@@ -1,5 +0,0 @@
-Feature: As a API user, I want to manage users
-
-  Scenario: Create new user
-    When I add new user with unique email
-    Then I should receive a details for him
